dd9c25cc2d683a0cc2948e3dfe218728a5afdf91
florian-ciuculescu <florian.ciuculescu@mastercard.com>
2021-08-22 09:08:06 +0100

    added different di examples using springframework

--- src/main/java/springframework/di/controllers/ConstructorInjectedController.java
+++ src/main/java/springframework/di/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package springframework.di.controllers;
 
+import org.springframework.stereotype.Controller;
 import springframework.di.services.GreetingService;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
--- src/main/java/springframework/di/controllers/SetterInjectedController.java
+++ src/main/java/springframework/di/controllers/SetterInjectedController.java
@@ -1,16 +1,20 @@
 package springframework.di.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import springframework.di.services.GreetingService;
 
+@Controller
 public class SetterInjectedController {
 
     private GreetingService greetingService;
 
-    public void setGreetingService(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
-
     public String getGreeting() {
         return greetingService.sayGreeting();
+    }
+
+    @Autowired
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
     }
 }

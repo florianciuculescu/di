e3409af666c13ab30ff8046e3f290ecc2552d305
florian-ciuculescu <florian.ciuculescu@mastercard.com>
2021-08-22 07:46:58 +0100

    added a di example

--- src/main/java/springframework/di/DiApplication.java
+++ src/main/java/springframework/di/DiApplication.java
@@ -2,12 +2,21 @@ package springframework.di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import springframework.di.controllers.MyController;
 
 @SpringBootApplication
 public class DiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiApplication.class, args);
+        ApplicationContext context = SpringApplication.run(DiApplication.class, args);
+
+        // spring context gaseste bean-urile dupa numele clasei (cu litera mica)
+        MyController myController = (MyController) context.getBean("myController");
+
+        String greeting = myController.sayHello();
+
+        System.out.println(greeting);
     }
 
 }

53eaa9b75fdae1a9e2b32643c4203d863150e201
florian-ciuculescu <florian.ciuculescu@mastercard.com>
2021-08-22 08:11:45 +0100

    added different di examples

---
+++ src/main/java/springframework/di/controllers/PropertyInjectedController.java
@@ -0,0 +1,12 @@
+package springframework.di.controllers;
+
+import springframework.di.services.GreetingService;
+
+public class PropertyInjectedController {
+
+    public GreetingService greetingService;
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/main/java/springframework/di/controllers/SetterInjectedController.java
@@ -0,0 +1,16 @@
+package springframework.di.controllers;
+
+import springframework.di.services.GreetingService;
+
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}

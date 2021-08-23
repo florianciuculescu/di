c4dabdf374de72c793f599e8de695c981ea1ffc6
florian-ciuculescu <florian.ciuculescu@mastercard.com>
2021-08-23 10:39:09 +0100

    used Qualifier

--- src/test/java/springframework/di/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/springframework/di/controllers/PropertyInjectedControllerTest.java
@@ -2,9 +2,7 @@ package springframework.di.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import springframework.di.services.GreetingServiceImpl;
-
-import static org.junit.jupiter.api.Assertions.*;
+import springframework.di.services.ConstructorGreetingService;
 
 class PropertyInjectedControllerTest {
 
@@ -15,7 +13,7 @@ class PropertyInjectedControllerTest {
 
         //mimicking what SpringFramework would do
         controller = new PropertyInjectedController();
-        controller.greetingService = new GreetingServiceImpl();
+        controller.greetingService = new ConstructorGreetingService();
     }
 
     @Test

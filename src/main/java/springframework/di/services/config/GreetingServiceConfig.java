8ada41cc46818e2366ff936d4f3f6688a1abb59b
florian-ciuculescu <florian.ciuculescu@mastercard.com>
2021-08-25 08:05:11 +0100

    Used Primary annotation in Java Based config

--- src/main/java/springframework/di/services/config/GreetingServiceConfig.java
+++ src/main/java/springframework/di/services/config/GreetingServiceConfig.java
@@ -2,12 +2,35 @@ package springframework.di.services.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import springframework.di.services.ConstructorGreetingService;
+import springframework.di.services.I18nEnglishGreetingService;
+import springframework.di.services.I18nSpanishGreetingService;
+import springframework.di.services.PrimaryGreetingService;
 import springframework.di.services.PropertyInjectedGreetingService;
 import springframework.di.services.SetterInjectedGreetingService;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService() {
+        return new I18nSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
